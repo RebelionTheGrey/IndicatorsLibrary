cb5653619acf1612fd517ff62136375875a8a7db
Rebelion <mitek_bobrovskij@inbox.ru>
2016-02-26 02:21:30 +0300

    Some improvements

--- IndicatorsLibrary/R based indicators/RLengthIndicator.cs
+++ IndicatorsLibrary/R based indicators/RLengthIndicator.cs
@@ -19,21 +19,25 @@ using System.Xml.Linq;
 using System.Xml.Serialization;
 
 
-using RManaged;
+using RManaged.Communications;
 using RManaged.Core;
-using BaseTypes;
+using RManaged.BaseTypes;
+using RManaged.Utilities;
 
 using RDotNet;
 using RDotNet.Internals;
 
 namespace IndicatorsLibrary.RBasedIndicators
 {
+    using BaseTypes;
+
     public abstract class RLenghtIndicator<T> : LengthIndicator<T>
     {
         protected IREngine Engine { get; set; }
 
         protected Script MainScript { get; set; }
         protected List<Script> SubScripts { get; set; }
+        protected List<Parameter> ScriptParameters { get; set; }        
 
         protected void LoadScripts(string scriptConfigName)
         {
@@ -42,8 +46,23 @@ namespace IndicatorsLibrary.RBasedIndicators
             var mainScriptName = config.Descendants("MainScriptName").Select(elem => { return elem.Value; }).ElementAt(0);
             var subScriptNames = config.Descendants("SubScriptNames").ToList();
 
+            var scriptXMLParams = config.Descendants("Parameters").ToList();
+            scriptXMLParams.ForEach(elem =>
+            {
+                var currentParameter = new Parameter();
+                var serializedAttr = elem.Attributes().First(attr => attr.Name.LocalName.CompareTo("Serialized") == 0);
+
+                currentParameter.IsSerialized = bool.Parse(serializedAttr.Value);
+
+                currentParameter.Name = elem.Name.LocalName;
+                currentParameter.Value = elem.Value;
+
+                ScriptParameters.Add(currentParameter);
+            });
+
             MainScript = new Script(mainScriptName, null);
             SubScripts = new List<Script>();
+            ScriptParameters = new List<Parameter>();
 
             subScriptNames.ForEach(elem =>
             {
--- IndicatorsLibrary/R based indicators/SimpleWaveletKalmanFilter.cs
+++ IndicatorsLibrary/R based indicators/SimpleWaveletKalmanFilter.cs
@@ -5,19 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 using IndicatorsLibrary.PlainIndicators;
-using RManaged;
+using RManaged.BaseTypes;
 using RManaged.Core;
+
 
 namespace IndicatorsLibrary.RBasedIndicators
 {
     public sealed class SimpleWaveletKalmanFilter : RLenghtIndicator<decimal>
     {
         private int windowSize;
-        private int level;
+        private int highFreqLevel;
+        private int lowFreqLevel;
 
         private RecursiveKalmanFilter kalmanFilter;
 
-        public SimpleWaveletKalmanFilter(int xDim, int yDim, float fadingCoeff, int level, int windowSize) :base(xDim, yDim, fadingCoeff)
+        public SimpleWaveletKalmanFilter(int xDim, int yDim, float fadingCoeff, int highFreqLevel, int lowFreqLevel, int windowSize, IREngine engine, string scriptConfigName) :base(engine, scriptConfigName)
         {
 
         }
