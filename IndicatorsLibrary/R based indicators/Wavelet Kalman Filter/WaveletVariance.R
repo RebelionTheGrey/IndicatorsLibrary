272f0110f0b6c94619dc3db460ad2fab2a83e962
Rebelion <mitek_bobrovskij@inbox.ru>
2016-02-26 03:36:05 +0300

    some improvements

--- IndicatorsLibrary/R based indicators/Wavelet Kalman Filter/WaveletVariance.R
+++ IndicatorsLibrary/R based indicators/Wavelet Kalman Filter/WaveletVariance.R
@@ -1,1 +1,13 @@
-﻿
+﻿WaveletMODWTVariance <- function(data, waveletType)
+{
+    library(wmtsa);
+
+    windowSize <- length(data);
+
+    maxDecompositionLvl <- ilogb(windowSize, base = 2);
+    transformedData <- wavMODWT(data, keep.series = FALSE, wavelet = waveletType);
+    waveletVarianceLevels <- summary(transformedData);
+
+    WaveletMODWTVariance <- waveletVarianceLevels$smat[1:(maxDecompositionLvl + 1), 8];
+    WaveletMODWTVariance
+}
